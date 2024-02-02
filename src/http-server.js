c5841efdd8cbb52d06f82ab658f1cf02740a4b3d
DmitriyYankovskiy <dmityank@gmail.com>
2024-02-02 23:50:33 +0300

    4343

--- src/http-server.js
+++ src/http-server.js
@@ -1,16 +1,18 @@
 const PORT = 9999;
 const HOST = globalThis.host;
 
-const http = require("http");
-const express = require("express");
+const HTTP = require("http");
+const EXPRESS = require("express");
 
-const app = express();
+let APP = express();
 
-app.use(express.static("/public"));
+app.set("view engine", "hbs");
+
+app.use(EXPRESS.static("/public"));
 
 app.use("/", require("../http-controllers/index.js"));
 
-let server = http.createServer(app);
+let server = HTTP.createServer(app);
 
 server.listen(PORT, HOST, () => {
     console.log(`http server was started on port: ${PORT}`);
